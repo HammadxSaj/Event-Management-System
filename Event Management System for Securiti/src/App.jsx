0165af985336d948663bdb6a318053f3b28014ed
Hammad Sajid <hs07606@st.habib.edu.pk>
2024-06-20 10:52:25 +0500

    Implemented the whole Add Event Form

--- Event Management System for Securiti/src/App.jsx
+++ Event Management System for Securiti/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import SignIn from './components/auth/SignIn'
 import SignUp from './components/auth/SignUp'
 import Home from './components/Home/Home'
+import AddEventButton from './components/admin/AddEventButton';
+import EventForm from './components/admin/EventForm';
 import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 
@@ -13,21 +15,24 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 function App() {
   const [count, setCount] = useState(0);
 
-  return (<BrowserRouter>
+  return (
+  <LocalizationProvider dateAdapter={AdapterDayjs}>
+  <BrowserRouter>
 
     <CssBaseline />
     
 
     <Routes>
       {/* set the initial route to Home */}
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<AddEventButton />} />
       <Route path="/signin" element={<SignIn />} />
       <Route path="/signup" element={<SignUp />} />
+      <Route path="/eventform" element={<EventForm />} />
     </Routes>
 
   </BrowserRouter>
+  </LocalizationProvider>
   )
 }
 
 export default App;
-
